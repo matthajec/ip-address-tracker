08cc5e55630cf9984bc03229742f4fe77e35bd3d
matthajec <66030008+matthajec@users.noreply.github.com>
2020-09-20 17:45:22 -0400

    finished for desktop
    added the map and made minor styling changes

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
 const $form = document.querySelector('.ip-input')
-
 let ip = ''
 let error = false
 
+//this creates a leafletjs map
+var map = L.map('map');
+
+L.tileLayer(`https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=tIc1i1kUs9AT4rg7mh1B`, {
+    tileSize: 512,
+    zoomOffset: -1,
+    minZoom: 1,
+    crossOrigin: true
+}).addTo(map);
+
+
+
+//initialize with an IP for google
 loadIP('8.8.8.8')
+
+//listens for a submit
+$form.addEventListener('submit', ev => {
+    ev.preventDefault()
+
+    loading(true)
+    //sets ip to the user inputted value
+    loadIP($form.elements['ip'].value)
+})
+
+//loads the info for the ip and displays it
+function loadIP(ip) {
+    getIPInfo(ip).then(info => {
+        //update the displayed info if there is no error, if there is clear the content
+        if (error) {
+            document.querySelectorAll('.ip-property-value').forEach(el => {
+                el.innerHTML = ''
+            })
+        } else {
+            updateIPInfo(info)
+        }
+        console.log(info)
+    })
+}
 
 //gets data from the ipify api
 async function getIPInfo(ip) {
 
     let type = ''
 
-    //if the input contains letters but not a : (for ipv6 address) then it must a domain, if not it's an ip
+    //changes the api url depending on whether it's a domain or an ip
     if (ip.toLowerCase().match(".*[a-z].*") && !ip.includes(':')) {
         type = 'domain'
     } else {
@@ -20,7 +56,7 @@ async function getIPInfo(ip) {
     console.log(ip)
     let res = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_Wnsrnezdn0bKFv0UaxLezbhSOKoXQ&${type}=${ip}`)//dont touch it :) it's a free account anyways
 
-    //set error to true or false depending on if the promise is ok
+    //handle an error
     if (!res.ok) {
         error = true
         //alerts the user there is an issue
@@ -34,10 +70,12 @@ Did you input a valid IP/domain? If you're sure the IP/domain is valid, try agai
     return data
 }
 
-//puts the ip information into the dom
+//outputs the ip information into the dom
 function updateIPInfo(info) {
     //stops the loading animation
     loading(false)
+    map.setView([info.location.lat, info.location.lng], 17)
+
     document.querySelector('.ip-address').textContent = info.ip
     document.querySelector('.ip-location').textContent = `${info.location.city}, ${info.location.region} ${info.location.postalCode}`
     document.querySelector('.ip-timezone').textContent = `UTC${info.location.timezone}`
@@ -62,27 +100,7 @@ function loading(status) {
     }
 }
 
-//loads the info for the ip and displays it
-function loadIP(ip) {
-    getIPInfo(ip).then(info => {
-        //handles errors
-        if (error) {
-            //resets the displayed text to nothing
-            document.querySelectorAll('.ip-property-value').forEach(el => {
-                el.innerHTML = ''
-            })
-        } else {
-            updateIPInfo(info)
-        }
-    })
+function fixMap(lat, lng) {
+    map.panTo(lat, lng)
+    map.setZoom(10)
 }
-
-//listens for a submit
-$form.addEventListener('submit', ev => {
-    ev.preventDefault()
-
-    loading(true)
-    //sets ip to the user inputted value
-    loadIP($form.elements['ip'].value)
-})
-
